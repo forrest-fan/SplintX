3cef248c9dac40a26f304a67c90bf82f2e5f73be
Felix He <felixhe.ski@outlook.com>
2020-07-13 13:27:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,45 @@ class App extends React.Component {
       page: newPage
     });
   }
+  //calls the Log in function when the page is first loaded to check if user is logged in.
+  window.onload = isLoggedIn();
+
+  function isLoggedIn() {
+    if(localStorage.getItem('username')) {
+      //signed in
+      $('#username').replaceWith("<div id=\"loggedin\">You are logged in as: "+ localStorage.getItem('username') +"</div>");
+      document.getElementById("button").innerHTML = "Log Out";
+      document.getElementById("button").onclick = logout;
+    }
+  }
+
+  function login() {
+    var username = document.getElementById("username").value;
+    //alert(username);
+    message = username + Date.now();
+    if(window.hive_keychain) {
+      //alert("has keychain");
+      hive_keychain.requestSignBuffer(username, message, "Posting", function(response) {
+        console.log(response);
+        var success = response["success"];
+        if (success) {
+          localStorage.setItem('username', username);
+          $('#username').replaceWith("<div id=\"loggedin\">You are logged in as: "+ localStorage.getItem('username') +"</div>");
+          document.getElementById("button").innerHTML = "Log Out";
+          document.getElementById("button").onclick = logout;
+       }
+     })
+   }
+   return false;
+  }
+  function logout() {
+    //alert("logged out");
+    $('#loggedin').replaceWith("<input type=\"text\" class=\"form-control\" placeholder=\"Hive Username\" id=\"username\">");
+    //document.getElementById("username").value = "";
+    document.getElementById("button").innerHTML = "Log In";
+    document.getElementById("button").onclick = login;
+    localStorage.removeItem("username");
+  }
 
   renderPage(page) {
     if (page === 'market') {
