b4e2cffb3b7ca7ec52ec06fd30716b2cb61ef879
unknown <forrest.c.fan@gmail.com>
2020-08-25 11:40:14 -0400

    add confirm for burn

--- src/Components/Collection/CollectionList/CollectionCard/CollectionCardModal/CollectionCardModal.js
+++ src/Components/Collection/CollectionList/CollectionCard/CollectionCardModal/CollectionCardModal.js
@@ -275,73 +275,80 @@ class Collectionmodal extends React.Component {
 
 	burn() {
 		let eligible = true;
+		let totalBurn = 0;
+		let totalCards = 0;
 		let selected = this.state.selected.map(card => {
 			if (card.leased || card.listed) {
 				eligible = false;
 			}
+			totalBurn += this.getBurn(card.bcx, card.xp);
+			totalCards++;
 			return card.uid;
 		});
-		if (!eligible) {
-			let toast = document.getElementById('ineligible-toast');
-			toast.className += ' show';
-			setTimeout(() => {toast.className = toast.className.replace(' show', '')}, 3000);
-		} else {
-			this.setState({
-				renderProgress: true,
-				progressMsg: 'Broadcasting request to the blockchain'
-			});
-			let burnJSON = JSON.stringify({
-				cards: selected,
-				app: 'steemmonsters/0.7.34'
-			});
-			window.hive_keychain.requestCustomJson(localStorage.getItem('username'), 'sm_burn_cards', 'Active', burnJSON, 'Burn Card(s)', function(response) {
-				if (response.success) {
-					this.setState({progressMsg: 'Step 1 of 2 complete - Request successfully broadcasted'}, () => {
+		let proceed = window.confirm("Burn " + totalCards + ' card(s) for ' + totalBurn + ' DEC?');
+		if (proceed) {
+			if (!eligible) {
+				let toast = document.getElementById('ineligible-toast');
+				toast.className += ' show';
+				setTimeout(() => {toast.className = toast.className.replace(' show', '')}, 3000);
+			} else {
+				this.setState({
+					renderProgress: true,
+					progressMsg: 'Broadcasting request to the blockchain'
+				});
+				let burnJSON = JSON.stringify({
+					cards: selected,
+					app: 'steemmonsters/0.7.34'
+				});
+				window.hive_keychain.requestCustomJson(localStorage.getItem('username'), 'sm_burn_cards', 'Active', burnJSON, 'Burn Card(s)', function(response) {
+					if (response.success) {
+						this.setState({progressMsg: 'Step 1 of 2 complete - Request successfully broadcasted'}, () => {
+							setTimeout(() => {
+								this.setState({progressMsg: 'Gathering request results.'});
+							}, 2000);
+						});					
+						let id = response.result.id;
+						let url = 'https://game-api.splinterlands.io/transactions/lookup?trx_id=' + id;
 						setTimeout(() => {
-							this.setState({progressMsg: 'Gathering request results.'});
-						}, 2000);
-					});					
-					let id = response.result.id;
-					let url = 'https://game-api.splinterlands.io/transactions/lookup?trx_id=' + id;
-					setTimeout(() => {
-						$.ajax({
-							type: 'GET',
-				  			url: url,
-				  			jsonpCallback: 'testing',
-				  			dataType: 'json',
-							success: function(response) {
-								if (response.error) {
-									this.setState({renderProgress: false});
-									let toast = document.getElementById('cardsFailed-toast');
-									toast.innerHTML = '<i class=\'fas fa-times\'></i> There was an error: ' + response.error;
-									toast.className += ' show';
-									setTimeout(() => {toast.className = toast.className.replace(' show', '')}, 3000);
-								} else {
-									this.setState({renderProgress: false});
-									let toast = document.getElementById('cardsBurned-toast');
-									toast.className += ' show';
-									setTimeout(() => {toast.className = toast.className.replace(' show', '')}, 3000);
-									this.props.updateBalance();
-									if (this.state.selected.length === this.state.cards.length) {
-										this.props.closeModal();
+							$.ajax({
+								type: 'GET',
+					  			url: url,
+					  			jsonpCallback: 'testing',
+					  			dataType: 'json',
+								success: function(response) {
+									if (response.error) {
+										this.setState({renderProgress: false});
+										let toast = document.getElementById('cardsFailed-toast');
+										toast.innerHTML = '<i class=\'fas fa-times\'></i> There was an error: ' + response.error;
+										toast.className += ' show';
+										setTimeout(() => {toast.className = toast.className.replace(' show', '')}, 3000);
+									} else {
+										this.setState({renderProgress: false});
+										let toast = document.getElementById('cardsBurned-toast');
+										toast.className += ' show';
+										setTimeout(() => {toast.className = toast.className.replace(' show', '')}, 3000);
+										this.props.updateBalance();
+										if (this.state.selected.length === this.state.cards.length) {
+											this.props.closeModal();
+										}
+										this.props.updateCollection('remove', selected);
+										this.setState({selected: []});
 									}
-									this.props.updateCollection('remove', selected);
-									this.setState({selected: []});
+								}.bind(this),
+								error: function(e) {
+									console.log('Something went wrong');
 								}
-							}.bind(this),
-							error: function(e) {
-								console.log('Something went wrong');
-							}
-						});
-					}, 10000);
-				} else {
-					this.setState({renderProgress: false});
-					let toast = document.getElementById('cardsFailed-toast');
-					toast.innerHTML = '<i class=\'fas fa-times\'></i> There was an error broadcasting to the blockchain.';
-					toast.className += ' show';
-					setTimeout(() => {toast.className = toast.className.replace(' show', '')}, 3000);
-				}
-			}.bind(this));
+							});
+						}, 10000);
+					} else {
+						this.setState({renderProgress: false});
+						let toast = document.getElementById('cardsFailed-toast');
+						toast.innerHTML = '<i class=\'fas fa-times\'></i> There was an error broadcasting to the blockchain.';
+						toast.className += ' show';
+						setTimeout(() => {toast.className = toast.className.replace(' show', '')}, 3000);
+					}
+				}.bind(this));
+			}
 		}
 	}
 
